82d3258e312741c1ca066912b568fa19a1d969f9
EugeneGoode <66566549+EugeneGoode@users.noreply.github.com>
2021-12-30 02:40:47 +0200

    Commit

--- hello.c
+++ hello.c
@@ -1,100 +1,67 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/printk.h>
-#include <linux/types.h>
+#include <linux/moduleparam.h>
 #include <linux/ktime.h>
 #include <linux/slab.h>
+#include <asm-generic/bug.h>
 
-
-MODULE_AUTHOR("Volodymyr Makotkin <makotk11n@ukr.net>");
-MODULE_DESCRIPTION("lab-5 linux kernel module");
+MODULE_AUTHOR("Zhenya Hudenko <zhenyahudenko@gmail.com>");
+MODULE_DESCRIPTION("Lab 6");
 MODULE_LICENSE("Dual BSD/GPL");
 
-
-
-struct head_list
-{
-    struct head_list* next;
-    ktime_t start_time;
-    ktime_t end_time;
-};
-
-
-static struct head_list* head;
 static uint counter = 1;
 
 module_param(counter, uint, 0444);
 MODULE_PARM_DESC(counter, "the number of hello world");
 
+struct my_struct {
+	struct list_head tlist;
+	ktime_t time;
+};
 
+static LIST_HEAD(my_list_head);
 
 static int __init hello_init(void)
-{
-    uint i = 0;
-    struct head_list *this_var, *next_var;
+{	
+	if (counter == 0) {
+		pr_warn("you cannot enter 0");
+	} else if (counter >= 5 && counter <= 10) {
+		pr_warn("The parameter is between 5 and 10");
+	}
 
-    pr_info("Input %d\n", counter);
-    if (!counter)
-    {
-        pr_info("You cannot enter 0");
-        pr_info("");
-        return 0;
-    }
+	BUG_ON((counter > 10));
 
-    if (counter >= 5 && counter <= 10)
-    {
-        pr_warn("You should enter number less than 5");
-    }
-    else
-    {
-        if (counter > 10)
-        {
-            pr_err("The parameter is greater than 10");
-            pr_info("");
-            return -EINVAL;
-        }
-    }
+	int i;
+	for (i = 0; i <= counter; i++) {
 
-    head = kmalloc(sizeof(struct head_list *), GFP_KERNEL);
-    this_var = head;
+		struct my_struct *ms = NULL;
 
-    for (i = 0; i < counter; i++)
-    {
-        this_var->next = kmalloc(sizeof(struct head_list), GFP_KERNEL);
-        this_var->start_time = ktime_get();
-        pr_info("Hello, world!\n");
-        this_var->end_time = ktime_get();
-        next_var = this_var;
-        this_var = this_var->next;
-    }
+		if (i != counter)
+		{
+			ms = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
+		}
+		ms->time = ktime_get();
+		list_add_tail(&ms->tlist, &my_list_head);
 
-    kfree(next_var->next);
-    next_var->next = NULL;
+		pr_info("Hello, world!\n");
+	}
 
-    return 0;
+	return 0;
 }
-
-
 
 static void __exit hello_exit(void)
 {
-    struct head_list *tmp_var;
+	int counter = 0;
+	struct my_struct *t, *tmp;
 
-    while (head != NULL && counter != 0)
-    {
-        tmp_var = head;
-        pr_info("Printing time: %lld",
-                tmp_var->end_time - tmp_var->start_time);
-        head = tmp_var->next;
-        kfree(tmp_var);
-    }
-
-    pr_info("Module removed");
-    pr_info("");
+	list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
+		pr_info("i: %d; ktime = %lld\n", counter++, ktime_to_ns(t->time));
+		list_del(&t->tlist);
+		kfree(t);
+	}
+	pr_info("Module removed\n");
 }
-
-
 
 module_init(hello_init);
 module_exit(hello_exit);
